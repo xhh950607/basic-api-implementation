ec6d99c02111908fda7af306b8e0768e4c58864e
xuhuihui <494655043@qq.com>
2020-08-04 17:59:43 +0800

    rs list test

---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.rslist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class RsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void should_return_rs_list_when_request_rs_list_in_get_method() throws Exception {
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
+                .andReturn();
+    }
+
+}
