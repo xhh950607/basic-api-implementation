58eb682365067f49be492ba9849660443f2847cd
xuhuihui <494655043@qq.com>
2020-08-05 15:28:07 +0800

    should bad request when add given invalid email

--- src/main/java/com/thoughtworks/rslist/domain/User.java
+++ src/main/java/com/thoughtworks/rslist/domain/User.java
@@ -3,10 +3,7 @@ package com.thoughtworks.rslist.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
@@ -20,6 +17,7 @@ public class User {
     private int age;
     @NotNull
     private String gender;
+    @Email
     private String email;
     private String phone;
 
